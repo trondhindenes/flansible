4ff16f2f4e92ea2051b325989eeba41303eb6c26
Trond Hindenes <trond@hindenes.com>
2016-06-08 22:57:30 +0200

    sh

--- Flansible/Utils/debug.sh
+++ Flansible/Utils/debug.sh
@@ -1,6 +1,10 @@
-killall screen
+echo kill screens if any
+killall screen || true
 cd ~/Flansible/Flansible
+echo start celery
 screen -d -m -s /bin/bash celery worker -A app.celery --loglevel=info
+echo start flower
 screen -d -m -s /bin/bash flower --broker=amqp://redis://localhost:6379/0
+echo start app
 screen -d -m -s /bin/bash /usr/bin/python app.py
 cd Utils
