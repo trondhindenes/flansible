ab8fbd9d067aa3644dd2913c4349049f199cb2c5
Trond Hindenes <trond@hindenes.com>
2016-06-08 23:02:53 +0200

    bugfix debg script

--- Flansible/Utils/debug.sh
+++ Flansible/Utils/debug.sh
@@ -4,7 +4,7 @@ cd ~/Flansible/Flansible
 echo start celery
 screen -d -m -s /bin/bash celery worker -A app.celery --loglevel=info
 echo start flower
-screen -d -m -s /bin/bash flower --broker=amqp://redis://localhost:6379/0
+screen -d -m -s /bin/bash flower --broker=redis://localhost:6379/0
 echo start app
 screen -d -m -s /bin/bash /usr/bin/python app.py
 cd Utils
