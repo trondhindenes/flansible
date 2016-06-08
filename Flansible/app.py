aa629e10d262cd5b10ede07e218199014e4923a9
Trond Hindenes <trond@hindenes.com>
2016-06-09 01:13:44 +0200

    configurable ip/port

--- Flansible/app.py
+++ Flansible/app.py
@@ -143,4 +143,4 @@ def do_long_running_task(self, cmd):
             return {'result': result}
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', use_reloader=False, port=80)
+    app.run(debug=True, host=config.get("Default", "Flask_tcp_ip"), use_reloader=False, port=int(config.get("Default", "Flask_tcp_port")))
