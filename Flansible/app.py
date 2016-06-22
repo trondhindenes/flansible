542f9b2694566463960e5b8c326572349c334704
Trond Hindenes <trond@hindenes.com>
2016-06-22 23:16:22 +0200

    better err handling

--- Flansible/app.py
+++ Flansible/app.py
@@ -356,18 +356,24 @@ class AnsibleTaskStatus(Resource):
             result = "Task not found"
             resp = app.make_response((result, 404))
             return resp
-        try:
-            return_code = task.info['returncode']
-            description = task.info['description']
-            if return_code is 0:
-                result_obj = {'Status': "SUCCESS", 
-                              'description': description}
-            else:
-                result_obj = {'Status': "ANSIBLEFAILURE",
-                              'description': description,
-                              'returncode': return_code}
-        except:
-            result_obj = {'Status': "CELERYFAILURE"}
+        elif task.state == 'PROGRESS':
+            result_obj = {'Status': "PROGRESS",
+                              'description': "Task is currently running",
+                              'returncode': None}
+        else:
+            try:
+                return_code = task.info['returncode']
+                description = task.info['description']
+                if return_code is 0:
+                    result_obj = {'Status': "SUCCESS", 
+                                  'description': description}
+                else:
+                    result_obj = {'Status': "ANSIBLEFAILURE",
+                                  'description': description,
+                                  'returncode': return_code}
+            except:
+                result_obj = {'Status': "CELERYFAILURE"}
+
         return  result_obj
 
 api.add_resource(AnsibleTaskStatus, '/api/ansibletaskstatus/<string:task_id>')
