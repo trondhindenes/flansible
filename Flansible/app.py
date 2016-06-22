312e0d4f850526b8fc935c3b5be89c0679bd05b9
Trond Hindenes <trond@hindenes.com>
2016-06-22 23:07:57 +0200

    bugfix

--- Flansible/app.py
+++ Flansible/app.py
@@ -14,6 +14,7 @@ import sys
 import json
 from threading import Thread
 from subprocess import Popen, PIPE
+import subprocess
 from Queue import Queue, Empty
 from datetime import datetime
 from ConfigParser import SafeConfigParser
