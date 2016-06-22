c13a71d52c871ad2e99fb8678e1b215da97c583f
Trond Hindenes <trond@hindenes.com>
2016-06-22 22:11:34 +0200

    bugfix

--- Flansible/app.py
+++ Flansible/app.py
@@ -387,7 +387,7 @@ def do_long_running_task(self, cmd):
                           meta={'result': output})
         print(str.format("About to execute: {0}", cmd))
         proc = Popen([cmd], stdout=PIPE, stderr=PIPE, shell=True)
-        for line in iter(process.stdout.readline, ''):
+        for line in iter(proc.stdout.readline, ''):
             print(str(line))
         #Thread(target=stream_watcher, name='stdout-watcher',
         #        args=('STDOUT', proc.stdout)).start()
