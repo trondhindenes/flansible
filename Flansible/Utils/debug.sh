712439cf8862dc7c047c7b83dee37eba93f61ccb
Trond Hindenes <trond@hindenes.com>
2016-08-13 18:44:49 +0200

    updated utils (celery script)

--- Flansible/Utils/debug.sh
+++ Flansible/Utils/debug.sh
@@ -2,7 +2,7 @@ echo kill screens if any
 killall screen || true
 cd /home/thadmin/flansible/
 echo start celery
-screen -d -m -s /bin/bash celery worker -A flansible.celery worker --loglevel=info
+screen -d -m -s /bin/bash celery worker -A flansible.celery --loglevel=info
 echo start flower
 screen -d -m -s /bin/bash flower --broker=redis://localhost:6379/0
 echo start app
